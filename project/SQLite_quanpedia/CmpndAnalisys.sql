6a09a01c8eaea9f07587b7eef906ffc0aa96ca5b
Neven Majdak <148250711+nevenmajdak@users.noreply.github.com>
2023-11-13 17:27:30 +0100

    initial commit

---
+++ project/SQLite_quanpedia/CmpndAnalisys.sql
@@ -0,0 +1,17 @@
+CREATE VIEW CmpndAnalisys AS
+    SELECT Analysis.Name AS Analysis,
+           Compound.Name AS Compound,
+           Compound.Formula AS Formula,
+           Compound.CompoundID
+      FROM (
+               Compound
+               INNER JOIN
+               IonDetails ON Compound.CompoundID = IonDetails.CompoundID
+           )
+           INNER JOIN
+           (
+               Analysis
+               INNER JOIN
+               CompoundsInAnalysis ON Analysis.AnalysisID = CompoundsInAnalysis.AnalysisID
+           )
+           ON IonDetails.IonDetailsID = CompoundsInAnalysis.IonDetailsID;
