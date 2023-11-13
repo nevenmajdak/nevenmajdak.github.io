0b9dd7648b0b8879f7c068a4570eb602f1f0382c
Neven Majdak <148250711+nevenmajdak@users.noreply.github.com>
2023-11-13 17:26:53 +0100

    initial commit

---
+++ project/SQLite_quanpedia/CompundNames.sql
@@ -0,0 +1,14 @@
+CREATE VIEW CmpNameParProd AS
+    SELECT CmpndAnalisys.Analysis AS Analysis,
+           CmpndAnalisys.Compound AS Name,
+           CmpndAnalisys.Formula AS Formula,
+           IonMode.Name AS IonMode,
+           ParentMass,
+           ProductMass,
+           CV,
+           CE
+      FROM MRMIonDetailsView,
+           IonMode,
+           CmpndAnalisys
+     WHERE MRMIonDetailsView.CompoundID = CmpndAnalisys.CompoundID AND 
+           IonMode.IonModeID = MRMIonDetailsView.IonModeID;
